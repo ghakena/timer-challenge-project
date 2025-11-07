e5dc4a53fa880d3a77f52fa837579703608731ae
Gibbs Herbert Akena <ghakena@gmail.com>
2025-11-07 04:37:50 +0300

    completed managing timer using a timer ref

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function TimerChallenge({ title, targetTime }) {
+    const timer = useRef();
     // state to store whether the timer has been started
     const [timerStarted, setTimerStarted] = useState(false);
     // state to know when timer duration has expired
     const [timerExpired, setTimerExpired] = useState(false);
-    let timer;
     // function to start timer when start challenge button is pressed.
     function handleStart() {
-        timer = setTimerStarted(true);
+        timer.current = setTimerStarted(true);
         setTimeout(() => {
             setTimerExpired(true);
         }, targetTime * 1000)
@@ -16,7 +16,7 @@ export default function TimerChallenge({ title, targetTime }) {
 
     // function to handle stopping timer when stop challenge button is pressed.
     function handleStop() {
-        clearTimeout(timer);
+        clearTimeout(timer.current);
     }
 
     return (
