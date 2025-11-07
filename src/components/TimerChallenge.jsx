c623e6ee24aedb2ae16c8591e44bd4667dc63ddc
Gibbs Herbert Akena <ghakena@gmail.com>
2025-11-07 04:12:10 +0300

    completed handleStart function plus all associated states

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,27 +1,33 @@
+import { useState } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
-    // state to know when timer expired
-    
-    
+    // state to store whether the timer has been started
+    const [timerStarted, setTimerStarted] = useState(false);
+    // state to know when timer duration has expired
+    const [timerExpired, setTimerExpired] = useState(false);
+
     // function to start timer when start challenge button is pressed.
     function handleStart() {
+        setTimerStarted(true);
         setTimeout(() => {
-
+            setTimerExpired(true);
         }, targetTime * 1000)
     }
 
     return (
         <section className="challenge">
             <h2>{title}</h2>
+            {timerExpired && <p>You lost!</p>}
             <p className="challenge-time">
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button>
-                    Start Challenge
+                <button onClick={handleStart}>
+                    {timerStarted ? 'Stop' : 'Start'} Challenge
                 </button>
             </p>
-            <p className="">
-                Time is running... / Timer inactive
+            <p className={timerStarted ? active : undefined}>
+                {timerStarted ? 'Time is running' : 'Timer inactive'}
             </p>
         </section>
     )
