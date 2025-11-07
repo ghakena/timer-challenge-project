2a042ffe62124a1ee3036bee8298d729de47df66
Gibbs Herbert Akena <ghakena@gmail.com>
2025-11-07 04:03:10 +0300

    started adding timer functionality and associated state

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,4 +1,14 @@
 export default function TimerChallenge({ title, targetTime }) {
+    // state to know when timer expired
+    
+    
+    // function to start timer when start challenge button is pressed.
+    function handleStart() {
+        setTimeout(() => {
+
+        }, targetTime * 1000)
+    }
+
     return (
         <section className="challenge">
             <h2>{title}</h2>
