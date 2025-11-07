dae99224a327b04af00a15ac83b2430f2a3451a6
Gibbs Herbert Akena <ghakena@gmail.com>
2025-11-07 04:20:53 +0300

    completed handleStop logic

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -5,17 +5,19 @@ export default function TimerChallenge({ title, targetTime }) {
     const [timerStarted, setTimerStarted] = useState(false);
     // state to know when timer duration has expired
     const [timerExpired, setTimerExpired] = useState(false);
-
+    let timer;
     // function to start timer when start challenge button is pressed.
     function handleStart() {
-        setTimerStarted(true);
+        timer = setTimerStarted(true);
         setTimeout(() => {
             setTimerExpired(true);
         }, targetTime * 1000)
     }
 
     // function to handle stopping timer when stop challenge button is pressed.
-    
+    function handleStop() {
+        clearTimeout(timer);
+    }
 
     return (
         <section className="challenge">
@@ -25,11 +27,11 @@ export default function TimerChallenge({ title, targetTime }) {
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button onClick={handleStart}>
+                <button onClick={timerStarted ? handleStop : handleStart}>
                     {timerStarted ? 'Stop' : 'Start'} Challenge
                 </button>
             </p>
-            <p className={timerStarted ? active : undefined}>
+            <p className={timerStarted ? "active" : undefined}>
                 {timerStarted ? 'Time is running' : 'Timer inactive'}
             </p>
         </section>
